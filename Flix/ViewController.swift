6e144e2466b038fa3638e8b0f62c977988fd1193
Pratistha Shrestha <pshrestha@headstorm.com>
2021-09-10 19:19:07 -0500

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Pratistha Shrestha on 9/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
